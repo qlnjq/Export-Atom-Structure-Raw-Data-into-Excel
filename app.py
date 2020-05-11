8b1e9480a03db6bef7440f697e7b7e25e05974bf
qlnjq <45474496+qlnjq@users.noreply.github.com>
2020-05-10 20:10:51 -0700

    first method,works

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 import requests
 from openpyxl import Workbook
 # import xlrd
+import csv
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -15,10 +17,18 @@ def index():
 @app.route('/upload_file', methods=['GET', 'POST'])
 def upload_file():
     if request.method == "POST":
-        rawData = request.files['rawFile'].read()
-        print(rawData)
-    # else:
-    #     return render_template(request, 'index.html')
+        data = []
+        reader = csv.reader(request.files['rawFile'].read().decode("utf-8").split('\n'), dialect='excel')
+        for row in reader:
+            if len(row) is not 0:
+                split_row = row[0].split(' ')
+                data.append(split_row)
+        print(data)
+
+        newData = pd.DataFrame(data, columns=['element', 'x', 'y', 'z']).set_index('element')
+        newData.to_csv("test.csv", sep=',')
+
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
